0f395d804f90f43b0f6acc8ee8a651d0a872c296
Charles Duran <62945929+4Charlie4@users.noreply.github.com>
2022-10-11 15:22:18 -0400

    images imported into webpack

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,3 +1,12 @@
 import "./form";
 import "./submit";
+import Logo from "../images/Charlie.png";
+import Bear from "../images/bear.png";
+import Dog from "../images/dog.png";
 
+
+window.addEventListener("load", function () {
+    document.getElementById("logo").src = Logo;
+    document.getElementById("bearThumbnail").src = Bear;
+    document.getElementById("dogThumbnail").src = Dog;
+})
