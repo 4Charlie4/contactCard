05db5777f7add90d54bbe2a6dbe9b8392de7aad9
Charles Duran <62945929+4Charlie4@users.noreply.github.com>
2022-10-12 13:38:09 -0400

    webpack added

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,12 +1,18 @@
+//js
 import "./form";
 import "./submit";
+//images
 import Logo from "../images/Charlie.png";
 import Bear from "../images/bear.png";
 import Dog from "../images/dog.png";
-
+//css
+import "../css/index.css";
+//bootstrap  & popper
+import { Tooltip, Toast, Popover } from "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 window.addEventListener("load", function () {
-    document.getElementById("logo").src = Logo;
-    document.getElementById("bearThumbnail").src = Bear;
-    document.getElementById("dogThumbnail").src = Dog;
-})
+  document.getElementById("logo").src = Logo;
+  document.getElementById("bearThumbnail").src = Bear;
+  document.getElementById("dogThumbnail").src = Dog;
+});
